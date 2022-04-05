02e09385eabd6ddb081c53b5390979a7afb18523
szq <814471424>
2022-04-05 13:08:04 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ use App\Http\Controllers\StaticPagesController;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
